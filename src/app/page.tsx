2b8085345639e67f4e5051e4ebb5ea33cc3c077e
talhacodespro <talha563280@gmail.com>
2026-02-04 00:42:50 +0600

    fix:issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { About } from '@/components/home/about';
 import { Header } from '@/components/home/header';
 import { Hero } from '@/components/home/hero';
-import { Projects } from '@/components/home/projects';
+// import { Projects } from '@/components/home/projects';
 import { Skills } from '@/components/home/skills';
 
 const Page = () => {
